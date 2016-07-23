367a7bc15dd97c7883c44f2e84741865637f679a
Hadrien Kohl <hadrien.kohl@ssb.no>
2016-07-23 16:58:36 +0200

    Avoid using the context to deserialize and add unit tests

--- src/main/java/no/ssb/jsonstat/v2/deser/DimensionDeserializer.java
+++ src/main/java/no/ssb/jsonstat/v2/deser/DimensionDeserializer.java
@@ -3,11 +3,11 @@ package no.ssb.jsonstat.v2.deser;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.google.common.base.Functions;
 import com.google.common.collect.*;
-import me.yanaga.guava.stream.MoreCollectors;
 import no.ssb.jsonstat.v2.Dimension;
 
 import java.io.IOException;
@@ -18,31 +18,36 @@ import java.util.stream.IntStream;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static me.yanaga.guava.stream.MoreCollectors.toImmutableMap;
 
 /**
  * Deserialize dimensions to {@link no.ssb.jsonstat.v2.Dimension.Builder}.
  */
-public class DimensionDeserializer extends JsonDeserializer<Dimension.Builder> {
+public class DimensionDeserializer extends StdDeserializer<Dimension.Builder> {
+
+    static final TypeReference<Map<String, String>> LABEL_MAP = new TypeReference<Map<String, String>>() {
+    };
+    static final TypeReference<List<String>> INDEX_LIST = new TypeReference<List<String>>() {
+    };
+    static final TypeReference<Map<String, Integer>> INDEX_MAP = new TypeReference<Map<String, Integer>>() {
+    };
+
+    public DimensionDeserializer() {
+        super(Dimension.Builder.class);
+    }
 
     @Override
     public Dimension.Builder deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+        // Get the name first.
+        String name = parseName(p, ctxt);
+
         if (p.getCurrentToken() == JsonToken.START_OBJECT) {
             p.nextToken();
         }
 
-        String name = parseName(p, ctxt);
         Dimension.Builder dimension;
         dimension = Dimension.create(name);
-
-        if (p.nextValue() != JsonToken.START_OBJECT)
-            ctxt.reportWrongTokenException(
-                    p,
-                    JsonToken.START_OBJECT,
-                    "dimension should be an object",
-                    (Object) null
-            );
-
-        p.nextValue();
 
         while (p.nextValue() != JsonToken.END_OBJECT) {
             switch (p.getCurrentName()) {
@@ -98,7 +103,7 @@ public class DimensionDeserializer extends JsonDeserializer<Dimension.Builder> {
         if (index == null) {
             checkArgument(label.size() >= 1, "category label must contain a least one element if " +
                     "no index is provided");
-            dimension.withCategories(ImmutableSet.copyOf(label.values()));
+            dimension.withIndexedLabels(ImmutableMap.copyOf(label));
             return;
         }
         if (label == null) {
@@ -118,7 +123,7 @@ public class DimensionDeserializer extends JsonDeserializer<Dimension.Builder> {
 
         ImmutableMap<String, String> withIndex = index.keySet().stream()
                 .collect(
-                        MoreCollectors.toImmutableMap(
+                        toImmutableMap(
                                 Function.identity(),
                                 label::get
                         )
@@ -134,28 +139,23 @@ public class DimensionDeserializer extends JsonDeserializer<Dimension.Builder> {
                     "label was not an object", (Object) null
             );
 
-        Map<String, String> label = ctxt.readValue(
-                p, ctxt.getTypeFactory().constructMapType(
-                        Map.class,
-                        String.class,
-                        String.class
-                )
-        );
+        Map<String, String> label = p.readValueAs(LABEL_MAP);
 
         return checkNotNull(label, "label object was null");
     }
 
-    private void parseUnit(JsonParser p, DeserializationContext ctxt) {
-
+    private void parseUnit(JsonParser p, DeserializationContext ctxt) throws IOException {
+        p.skipChildren();
     }
 
     /**
      * Extract the dimension name.
      */
     private String parseName(JsonParser p, DeserializationContext ctxt) throws IOException {
-        if (p.currentToken() != JsonToken.FIELD_NAME)
-            ctxt.reportWrongTokenException(p, JsonToken.FIELD_NAME,
-                    "could not determine dimension name", p.getCurrentToken());
+        // TODO: Investigate this. The current name is the key value and token is object.
+        //if (p.currentToken() != JsonToken.FIELD_NAME)
+        //    ctxt.reportWrongTokenException(p, JsonToken.FIELD_NAME,
+        //            "could not determine dimension name", p.getCurrentToken());
 
         String name = checkNotNull(
                 p.getCurrentName(),
@@ -167,7 +167,7 @@ public class DimensionDeserializer extends JsonDeserializer<Dimension.Builder> {
     }
 
     private String parseLabel(JsonParser p, DeserializationContext ctxt) throws IOException {
-        String label = ctxt.readValue(p, String.class);
+        String label = _parseString(p, ctxt);
         checkArgument(!label.trim().isEmpty(), "label cannot be empty");
         return label;
     }
@@ -177,47 +177,49 @@ public class DimensionDeserializer extends JsonDeserializer<Dimension.Builder> {
         // both to array.
         ImmutableMap<String, String> index = null;
         JsonToken token = p.currentToken();
-        if (token == JsonToken.START_ARRAY) {
-            List<String> listIndex = ctxt.readValue(
-                    p,
-                    ctxt.getTypeFactory().constructCollectionType(
-                            List.class,
-                            String.class
-                    )
-            );
-            index = IntStream.range(0, listIndex.size())
-                    .boxed()
-                    .collect(MoreCollectors.toImmutableMap(
-                            listIndex::get, Object::toString
-                    ));
-
-        } else if (token == JsonToken.START_OBJECT) {
-            Map<String, Integer> mapIndex = ctxt.readValue(
-                    p,
-                    ctxt.getTypeFactory().constructMapType(
-                            Map.class,
-                            String.class,
-                            Integer.class
-                    )
-            );
-            // Even though the type is String, the sorting actually uses the
-            // integer value thanks to the forMap function.
-            Ordering<String> byValue = Ordering.natural().onResultOf(
-                    Functions.forMap(mapIndex)
-            );
-
-
-            index = ImmutableSortedMap.copyOf(
-                    Maps.transformValues(mapIndex, Object::toString),
-                    byValue
-            );
-
-        } else
+        if (token == JsonToken.START_ARRAY)
+            index = parseIndexAsArray(p);
+        else if (token == JsonToken.START_OBJECT)
+            index = parseIndexAsMap(p);
+        else
             ctxt.reportMappingException("could not deserialize category index, need either an array " +
                     "or an object, got %s", token);
 
-        // TODO: If the index is a map, should it starts with 0?
         return checkNotNull(index, "could not parse index");
 
+    }
+
+    private ImmutableMap<String, String> parseIndexAsMap(JsonParser p) throws IOException {
+        ImmutableMap<String, String> index;
+
+        Map<String, Integer> mapIndex = p.readValueAs(
+                INDEX_MAP
+        );
+
+        // Even though the type is String, the sorting actually uses the
+        // integer value thanks to the forMap function.
+        Ordering<String> byValue = Ordering.natural().onResultOf(
+                Functions.forMap(mapIndex)
+        );
+
+
+        index = ImmutableSortedMap.copyOf(
+                Maps.transformValues(mapIndex, Object::toString),
+                byValue
+        );
+        return index;
+    }
+
+    private ImmutableMap<String, String> parseIndexAsArray(JsonParser p) throws IOException {
+        ImmutableMap<String, String> index;
+
+        List<String> listIndex = p.readValueAs(
+                INDEX_LIST);
+        index = IntStream.range(0, listIndex.size())
+                .boxed()
+                .collect(toImmutableMap(
+                        listIndex::get, Object::toString
+                ));
+        return index;
     }
 }
